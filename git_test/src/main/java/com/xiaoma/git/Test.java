b9c73d686d72ddf2ad20b9bb8be0d944a4b42f1c
1728813710@qq.com <Xiaoma666>
2021-05-04 14:27:46 +0800

    添加文件Test

---
+++ git_test/src/main/java/com/xiaoma/git/Test.java
@@ -0,0 +1,15 @@
+package com.xiaoma.git;
+
+/**
+ * @author xiaoma
+ * @class
+ * @create 2021-05-04-13:57
+ * @user Administrator
+ * @project git_workspace
+ * @package com.xiaoma.git
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
